36a78fc10606f6f0a3ce4fdcd11b06ca29fd215f
Coffee448 <andy_may_hazi_mejoresamigas@hotmail.com>
2022-04-25 00:07:27 -0500

    Nuevo personaje y avances en menu personajes

--- Escenas/Menus/Main_Menu.gd
+++ Escenas/Menus/Main_Menu.gd
@@ -5,3 +5,7 @@ func _on_Button_pressed():
 
 func _on_Button4_pressed():
 	get_tree().change_scene("res://Escenas/Menus/Modals_Menu.tscn")
+
+
+func _on_Button5_pressed():
+	get_tree().change_scene("res://Escenas/Niveles/Level.tscn")
