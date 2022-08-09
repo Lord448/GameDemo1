409011435556f5ae96ceb871d01236048e6982f2
Coffee448 <andy_may_hazi_mejoresamigas@hotmail.com>
2022-08-09 17:27:36 -0500

    Nuevo inicio, modificaciones a escenas de menu y corrección de dependencias

--- Escenas/Menus/Modals_Menu.gd
+++ Escenas/Menus/Modals_Menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Escenas/Menus/Save_Menu.tscn")
+	get_tree().change_scene("res://Escenas/Menus/Characters_Menu.tscn")
 
 
 func _on_Button4_pressed():
--- Gamehandler.gd
+++ Gamehandler.gd
@@ -3,6 +3,8 @@ extends Node
 var Velocidad = 500
 var Presiones
 var Stage
+var player_dir: String = "res://Escenas/Personajes/Player/Player.tscn"
+var player: Node2D
 
 func _ready():
 	pass
