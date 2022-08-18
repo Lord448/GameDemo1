8ceba4d8d0fa5779a635ed9c60c77f8a40c1c583
Lord448 <pedroeroca@outlook.com>
2022-08-17 21:22:52 -0500

    Arreglo de botones de stage

--- Escenas/Menus/Stage_menu.gd
+++ Escenas/Menus/Stage_menu.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var Escenario: String
 
 func _ready():
 	pass
@@ -20,5 +21,175 @@ func _on_Regresar_pressed():
 func _on_Stage_1_pressed():
 	for i in range(1,20):
 		get_node("Stage_" + String(i)).pressed = false
-	#$Stage_02.pressed = false
 	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_01.tscn"
+
+func _on_Stage_2_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_2":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_02.tscn"
+
+func _on_Stage_3_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_3":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_03.tscn"
+
+func _on_Stage_4_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_4":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_04.tscn"
+
+func _on_Stage_5_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_5":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_05.tscn"
+
+func _on_Stage_6_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_6":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_06.tscn"
+
+func _on_Stage_7_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_7":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_07.tscn"
+
+func _on_Stage_8_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_8":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_08.tscn"
+
+func _on_Stage_9_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_9":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_09.tscn"
+
+func _on_Stage_10_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_10":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_10.tscn"
+	
+func _on_Stage_11_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_11":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_11.tscn"
+
+func _on_Stage_12_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_12":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_12.tscn"
+
+func _on_Stage_13_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_13":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_13.tscn"
+
+func _on_Stage_14_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_14":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_14.tscn"
+
+func _on_Stage_15_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_15":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_15.tscn"
+
+func _on_Stage_16_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_16":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_16.tscn"
+
+func _on_Stage_17_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_17":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_17.tscn"
+
+func _on_Stage_18_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_18":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_18.tscn"
+
+func _on_Stage_19_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_19":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_19.tscn"
+
+func _on_Stage_20_pressed():
+	for i in range(1,20):
+		Escenario = "Stage_" + String(i)
+		if Escenario == "Stage_20":
+			pass
+		else:
+			get_node(Escenario).pressed = false
+	Gamehandler.Stage = "res://Escenas/Niveles/Niveles_Arcade/Stage_20.tscn"
